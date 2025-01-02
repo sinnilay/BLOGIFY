747d75b9947e2396953e4a9f2a03dcf5b217697a
sinnilay <singhnilay55@gmail.com>
2025-01-02 21:47:34 +0530

    CLODINARY ADDED

---
+++ Services/config.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dw9upx1zh',
+    api_key: '835521147779742',
+    api_secret: 'JcuAloEcsAQILnbYNiWvabvLOXo'
+});
+
+module.exports = cloudinary;
--- routes/blog.js
+++ routes/blog.js
@@ -3,23 +3,32 @@ const multer = require('multer')
 const path = require('path')
 const blog = require("../models/blog")
 const { title } = require('process')
-
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinaryconfig = require("../Services/config.js")
 const router= Router()
-function removeSpaces(str) {
-  return str.replace(/\s+/g, '');
-}
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinaryconfig,
+  params: {
+    folder: "BlogsImage", // Folder name in Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png"],
+  },
+});
+// function removeSpaces(str) {
+//   return str.replace(/\s+/g, '');
+// }
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
     
-      cb(null,"./public/uploads");
-    },
-    filename: function (req, file, cb) {
-      const spacefreename= removeSpaces(file.originalname)
-      const filename = `${Date.now()}-${spacefreename}`;
-      cb(null, filename);
-    }
-  });
-  const upload = multer({ storage: storage })
+//       cb(null,"./public/uploads");
+//     },
+//     filename: function (req, file, cb) {
+//       const spacefreename= removeSpaces(file.originalname)
+//       const filename = `${Date.now()}-${spacefreename}`;
+//       cb(null, filename);
+//     }
+//   });
+const upload = multer({ storage });
+
 router.get('/add-blog',(req,res)=>{
     res.render('Addzblog',{
         user:req.user
@@ -30,11 +39,12 @@ router.post('/add-blog',upload.single('coverimage'),async (req,res)=>{
   //  console.log(req.body);
   //  console.log(req.file); 
    const {title,body} = req.body;
+   const result = await cloudinaryconfig.uploader.upload(req.file.path);
    await blog.create({
        title,
        body,
        createdby: req.user.id,
-       ImageUrl: req.file.path
+       ImageUrl: result.secure_url
    })   
    res.redirect("/")
 
